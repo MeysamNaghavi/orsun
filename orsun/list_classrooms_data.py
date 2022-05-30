2ce862736a2cec8b7665323c23b7c9bf5ee98a5d
Meysam Naghavi <meysam.ngv@gmail.com>
2022-05-31 02:29:05 +0430

    add code to get all classrooms data as a python dict

---
+++ orsun/list_classrooms_data.py
@@ -0,0 +1,82 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import re
+
+
+my_header = {
+    "Host": "89.43.5.11:8082",
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept": "*/*",
+    "Referer": "http://89.43.5.11:8082/orsun/",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": 'en-US,en;q=0.9'
+}
+
+
+# find bj and id,which are required for the request.get
+def find_required(driver):
+    # find bj in dom
+    bj_link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.x-panel.x-abs-layout-item.x-rtl.x-panel-default.x-grid'))
+    ).get_attribute('id')
+    bj_link = bj_link[:bj_link.find('_')]
+
+    # find id in dom
+    id_link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'iframe'))
+    ).get_attribute('id')
+    id_link = id_link[id_link.find('_') + 1:]
+
+    return {'bj_link': bj_link, 'id_link': id_link}
+
+
+# create dict of classrooms data
+def json_creator(response):
+    # find where is start data
+    regex = re.compile(r"\"rows\":(\[.*])")
+    matches = eval(re.search(regex, response)[1])
+
+    # Create useful personalized data from extracted information
+    data = {}
+    for items in matches:
+        code_dars = items["1"]
+        classroom = [
+            {
+                'code': items["1"],
+                'name': items["2"],
+                'fname': items["3"],
+                'lname': items["4"],
+                'mobile': items["5"],
+                'degree': items["6"],
+                'exam_type': items["7"],
+                'city': items["8"],
+                'description': items["9"]
+            }
+        ]
+        if code_dars in data:
+
+            data[code_dars].append(classroom[0])
+
+        else:
+            data[code_dars] = classroom
+
+    return data
+
+
+# for requests to get all classrooms data
+def get_classrooms_data(driver, base_url):
+    bj_and_id = find_required(driver)
+    url = f'{base_url}hyper_server.dll/HandleEvent?IsEvent=1&Obj={bj_and_id["bj_link"]}' \
+          f'&Evt=data&_S_ID={bj_and_id["id_link"]}&options=1&page=1&start=0&limit=25 '
+
+    req = requests.get(url, headers=my_header)
+
+    if req.ok:
+        return json_creator(response=req.text)
+    else:
+        print(f'error in requests : {req.status_code}')
