2293dc04204ef6beb8b9069b40411fac5c5897bf
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-02 01:39:48 +0430

    rename loading.py and fix class name . bug

--- orsun/loading.py
+++ orsun/loading.py
@@ -7,7 +7,7 @@ from time import sleep
 # check loading class count if is == 1 then continue
 def check(driver):
     while True:
-        loadings = driver.find_elements(By.CLASS_NAME, '.x-mask-msg-text.x-rtl')
+        loadings = driver.find_elements(By.CLASS_NAME, 'x-mask-msg-text.x-rtl')
         if len(loadings) != 1:
             sleep(1)
         else:
