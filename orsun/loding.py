24e50f020c57e91d0e01dc655fd62735fc691503
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-02 01:15:29 +0430

    add site loading check alert code

---
+++ orsun/loding.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+# check loading class count if is == 1 then continue
+def check(driver):
+    while True:
+        loadings = driver.find_elements(By.CLASS_NAME, '.x-mask-msg-text.x-rtl')
+        if len(loadings) != 1:
+            sleep(1)
+        else:
+            break
