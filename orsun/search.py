b8dc8d451a7c401ff957da66856d8c93e21c2be0
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-02 11:53:54 +0430

    bugfix/fast non-stop search

--- orsun/search.py
+++ orsun/search.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from orsun import loading
+from time import sleep
 
 
 # find id in search box to driver able click,send_key to it
@@ -17,14 +18,16 @@ def find(driver, lesson_code):
     search_box_xpath = f'//*[@id="{find_required(driver)}-inputEl"]'
     search = driver.find_element(By.XPATH, search_box_xpath)
 
-    # click on search box and clear filed
+    # click on search box and clear filed then sleep 0.5 second
     search.click()
     search.clear()
+    sleep(0.5)
 
     # sleep if loadings... appear
     loading.check(driver)
 
-    # send_key lesson_code
+    # send_key lesson_code then sleep 0.5 second
     search.send_keys(lesson_code)
+    sleep(0.5)
 
     loading.check(driver)
