49ef480ed433884a596b9c2dc7f8491e7724ab2a
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-02 14:02:05 +0430

    add matching function that can filter search and return selenium WebElement only lesson_code is the same + bugfix find_required using WebDriverWait

--- orsun/search.py
+++ orsun/search.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from orsun import loading
 from time import sleep
@@ -5,8 +7,11 @@ from time import sleep
 
 # find id in search box to driver able click,send_key to it
 def find_required(driver):
-    element = driver.find_element(By.CLASS_NAME, 'x-form-trigger.x-form-trigger-default.x-form-search-trigger.x-form'
-                                                 '-search-trigger-default.x-rtl')
+    loading.check(driver)
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'x-form-trigger.x-form-trigger-default.x-form-search-trigger.x'
+                                                   '-form-search-trigger-default.x-rtl'))
+    )
 
     id = element.get_attribute('id')
     id = id[:id.find('id')+2]
@@ -31,3 +36,26 @@ def find(driver, lesson_code):
     sleep(0.5)
 
     loading.check(driver)
+
+
+def matching(driver, lesson_code):
+    # List of items found that match the lesson code
+    match = []
+
+    items_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'x-grid-view.x-grid-with-col-lines.x-grid-with-row-lines.x-fit'
+                                                       '-item.x-rtl.x-grid-view-default.x-unselectable.x-scroller'))
+    )
+
+    items = items_box.find_elements(By.TAG_NAME, 'table')
+
+    for i in range(1, len(items) + 1):
+        lesson_code_in_table = f'/html/body/div[5]/div/div/div/div/div/div/div/div[1]/div/div/div/div/div[2]' \
+                               f'/div[2]/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div[2]' \
+                               f'/table[{i}]/tbody/tr/td[3]/div'
+        if lesson_code in driver.find_element(By.XPATH, lesson_code_in_table).text:
+            match.append(items[i - 1])
+            print(items[i - 1].text)
+
+    # return selenium WebElement
+    return match
