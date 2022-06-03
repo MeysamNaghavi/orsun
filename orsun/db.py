7f6c4e1dbac880f8136044a1f27145a51db8d98c
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-03 17:42:45 +0430

    add save_classroom_details() Which sets a record for classroom details + lesson code in the meeting table

--- orsun/db.py
+++ orsun/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 
 def save_classrooms_db(data):
@@ -22,3 +23,20 @@ def save_classrooms_db(data):
                            ':teacher_lname ,:teacher_mobile ,:degree ,:exam_type ,:city ,:description)', data)
 
     print("Database created successfully")
+
+
+# save classroom details for one lesson_code in table meetings
+def save_classroom_details(lesson_code, data):
+    meetings_details = json.dumps(data, ensure_ascii=False)
+
+    with sqlite3.connect('database.db') as connections:
+
+        cursor = connections.cursor()
+
+        cursor.execute('''CREATE TABLE IF NOT EXISTS meetings
+                        (lesson_code text UNIQUE,
+                        meetings text)
+                        ''')
+
+        cursor.execute('INSERT OR REPLACE INTO meetings VALUES (?, ?)', (lesson_code, meetings_details))
+
