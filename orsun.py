d8fd3db7f2a3be2d3abe76fbd652d9f7cd50501f
Meysam Naghavi <meysam.ngv@gmail.com>
2022-06-09 02:47:55 +0430

    Lots of fixes, dirty code, but it works well :)

--- orsun.py
+++ orsun.py
@@ -1,4 +1,5 @@
-from selenium import webdriver
+from time import sleep
+from selenium.common import NoSuchElementException, StaleElementReferenceException
 from auth import login
 from orsun import classroom, list_classrooms_data
 from orsun import db
@@ -9,26 +10,23 @@ from selenium import webdriver
 config = {
     'password': None,
     'username': None,
-    'is_classrooms_table_created': False,
-    'is_login': False,
     'site_url': 'http://89.43.5.10/orsun/',
     'tables_name': db.get_tables_name(),
 }
 
-# set driver executable_path if not  find
 driver = webdriver.Firefox()
 driver.minimize_window()
+driver.get(config['site_url'])
+sleep(3)
+login.do_login(driver=driver, username=config.get('username'), password=config.get('password'))
+loading.check(driver)
+classroom.get_classrooms_section(driver)
 
-if config.get('username') or config.get('password') is None:
-    config['username'] = input('Enter Username : ')
-    config['password'] = input('Enter Password : ')
 
-
+# reload and login then go today classrooms section
 def refresh_process(lesson_code):
     driver.refresh()
-
-    loading.check(driver)
-
+    sleep(3)
     login.do_login(driver=driver, username=config.get('username'), password=config.get('password'))
     config['is_login'] = True
 
@@ -52,11 +50,12 @@ def refresh_process(lesson_code):
 
 
 # save all classrooms data in 'لیست کلاس های امروز'
-def save_all_classrooms_to_db():
-    if config['is_login'] is not True:
-        driver.get(config['site_url'])
-        login.do_login(driver=driver, username=config.get('username'), password=config.get('password'))
-        config['is_login'] = True
+def save_all_classrooms_to_db(driver):
+    loading.check(driver)
+
+    classroom.get_classrooms_section(driver)
+
+    loading.is_ready_to_sendkeys(driver)
 
     # save classrooms data to database
     data = list_classrooms_data.get_classrooms_data(driver=driver, base_url=config['site_url'])
@@ -69,25 +68,30 @@ def save_all_classrooms_to_db():
 
 
 # save details of all lesson codes available in the database.
-def save_all_classrooms_details_to_db():
-    if 'classrooms' not in config.get('tables_name'):
-        save_all_classrooms_to_db()
-        db.lesson_codes_for_search()
-        config['tables_name'] = db.get_tables_name()
+def save_all_classrooms_details_to_db(driver):
+    if 'codes' not in config.get('tables_name'):
+        if 'classrooms' not in config.get('tables_name'):
+            save_all_classrooms_to_db(driver)
+            config['tables_name'] = db.get_tables_name()
 
-    if 'code' not in config.get('code'):
         db.lesson_codes_for_search()
         config['tables_name'] = db.get_tables_name()
+        driver.refresh()
+        sleep(3)
+        login.do_login(driver=driver, username=config.get('username'), password=config.get('password'))
+        loading.check(driver)
+        classroom.get_classrooms_section(driver)
 
     saved_cunt = 0
     while True:
         loading.check(driver)
         lesson_code = db.get_one_code()
-
-        if type(lesson_code) is not None:
+        if lesson_code is not None:
             lesson_code = lesson_code[0]
+            loading.check(driver)
             search.find(driver, lesson_code)
 
+            loading.is_table_update(driver)
             loading.check(driver)
 
             match = search.matching(driver, lesson_code)
@@ -95,8 +99,9 @@ def save_all_classrooms_details_to_db():
             loading.check(driver)
             try:
                 classroom_details_list = classroom_details.get_details(driver, match, config['site_url'])
-            except:
-                refresh_process(lesson_code)
+            except NoSuchElementException or StaleElementReferenceException:
+                print('refresh_process\n')
+                classroom_details_list = refresh_process(lesson_code)
 
             loading.check(driver)
 
@@ -110,10 +115,69 @@ def save_all_classrooms_details_to_db():
         else:
             if saved_cunt == 0:
                 print('nothing to find and saved ! \n')
-                break
+                exit()
             else:
                 print(f'{saved_cunt} classrooms details saved in database')
-                break
+                exit()
 
 
+# search custom
+def custom_search_and_save(classrooms_code):
+    search_list = classrooms_code.split(',')
+    search_list = [code for code in search_list if len(code) == 7]
 
+    print(f'search and save valid lesson_code : {search_list}\n')
+    saved_cunt = 0
+    while len(search_list) != 0:
+        lesson_code = search_list[0]
+        loading.check(driver)
+        search.find(driver, lesson_code)
+
+        loading.is_table_update(driver)
+        loading.check(driver)
+
+        match = search.matching(driver, lesson_code)
+
+        loading.check(driver)
+        try:
+            classroom_details_list = classroom_details.get_details(driver, match, config['site_url'])
+        except NoSuchElementException or StaleElementReferenceException:
+            print('refresh_process\n')
+            classroom_details_list = refresh_process(lesson_code)
+
+        loading.check(driver)
+
+        db.save_classroom_details(lesson_code, classroom_details_list)
+
+        search_list.remove(lesson_code)
+        saved_cunt += 1
+
+    print(f'{saved_cunt} classrooms details saved in database')
+
+# 1 for save_all_classrooms_to_db
+# 2 for save_all_classrooms_details_to_db
+# 3 for custom_search_and_save
+# 4 for exit
+
+print('''
+\t*** select ***
+1 - save_all_classrooms_to_db
+2 - save_all_classrooms_details_to_db
+3 - custom_search_and_save
+4 - exit
+''')
+
+what_is_do = input('Enter : ')
+
+if what_is_do == '1':
+    save_all_classrooms_to_db(driver)
+elif what_is_do == '2':
+    driver.minimize_window()
+    save_all_classrooms_details_to_db(driver)
+elif what_is_do == '3':
+    codes = input("\nenter codes for custom search like 1,2,3 : ")
+    custom_search_and_save(codes)
+elif what_is_do == '4':
+    exit()
+else:
+    print('Error')
